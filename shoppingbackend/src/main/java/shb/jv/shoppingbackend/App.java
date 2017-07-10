c886c0bb668284e8efd019e7ff3ae24295cc1786
Shabab Akbar <info@shababakbar.com>
2017-07-10 21:19:43 +1000

    The project completed successfully.

---
+++ shoppingbackend/src/main/java/shb/jv/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package shb.jv.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
