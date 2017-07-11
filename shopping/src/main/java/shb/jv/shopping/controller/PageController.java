9b61ecfe03e8a35ffdefe713b668b5a97d19562f
Shabab Akbar <info@shababakbar.com>
2017-07-11 14:48:14 +1000

    successful

--- shopping/src/main/java/shb/jv/shopping/controller/PageController.java
+++ shopping/src/main/java/shb/jv/shopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package shb.jv.shopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,4 +14,25 @@ public class PageController {
 		mv.addObject("greeting", "Welcome to Spring Web MVC by Shabab Akbar");
 		return mv;
 	}
+	
+//	@RequestMapping("/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting){
+//		if (greeting==null){
+//			greeting="Hello there";
+//		}
+//		ModelAndView mv=new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+
+	@RequestMapping("/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting){
+		if (greeting==null){
+			greeting="Hello there";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+
 }
